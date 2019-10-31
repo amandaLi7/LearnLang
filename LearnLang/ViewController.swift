7cbf0201c0c99c6c3532f24f90ef89baee7335bf
Amanda Li <xinyue_amanda_li@yahoo.com>
2019-10-31 15:58:49 -0500

    Initial Commit

---
+++ LearnLang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnLang
+//
+//  Created by Xinyue Amanda Li on 10/31/19.
+//  Copyright © 2019 Xinyue Amanda Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
